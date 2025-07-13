804f193b99e8e59e201622a8ade596530f89b35d
[dyad] <git@dyad.sh>
2025-07-13 20:23:09 +0100

    [dyad] wrote 1 file(s)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } => 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { supabase } from '@/integrations/supabase/client';
 import { useSession } from '@/components/SessionContextProvider';
